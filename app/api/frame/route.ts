858621ef7d943b35aa5df702354e55cc3f415ba4
Isabel Bruda <diebrudie@Isabels-MacBook-Pro.local>
2024-02-07 18:08:56 +0100

    changed cta messages

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -12,9 +12,9 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         <title>Monster Nr. 4</title>
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qmd6A6cTZo1ULZybN9MirUgWTuEd6gfJhYVhouxZFGbWyG/0_0-midjourney-0${id}.png" />
-        <meta property="fc:frame:button:1" content="Open In My Pinata Account" />
+        <meta property="fc:frame:button:1" content="Check Out My Portfolio" />
         <meta property="fc:frame:button:1:action" content="post_redirect" />
-        <meta property="fc:frame:button:2" content="Create Your Own Frame" />
+        <meta property="fc:frame:button:2" content="Check Out A Random Project" />
         <meta property="fc:frame:button:2:action" content="post_redirect" />
         <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
         </head></html>`);
